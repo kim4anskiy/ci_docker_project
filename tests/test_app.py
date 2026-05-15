b4e573b64c3f7f440c429dc77c3cb5598f95de5b
Danil <prokopenkovd7@gmail.com>
2026-05-15 09:43:07 +0300

    добавлены тесты

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,3 +8,11 @@ def test_home(client):
 	response = client.get('/')
 	assert response.status_code == 200
 	assert response.data == b'Hello, Docker CI/CD!'
+
+def test_home_page_content_type(client):
+	response = client.get('/')
+	assert response.content_type = 'text/html; charset=utf-8'
+
+def test_post_method_not_allowed(client):
+	response = client.post('/')
+	assert response.status_code == 405
